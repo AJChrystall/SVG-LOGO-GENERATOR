247654ff62f3a2e6ab8feb6a68be4d5fdea8ea58
Alexander Chrystall <zander@MacBook-Air-4.local>
2023-05-02 21:03:34 +1000

    I am so close but still have a few SVG issues.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const SVG = require("svgwrite");
+const SVG = require("@svgdotjs/svg.js");
 
 async function generateLogo() {
   const answers = await inquirer.prompt([
@@ -30,35 +30,28 @@ async function generateLogo() {
   const { text, textColor, shape, shapeColor } = answers;
 
   // Create a new SVG document
-  const svg = SVG({
-    width: 300,
-    height: 200
-  });
+  const svg = SVG().size(300, 200);
 
   // Add the specified shape to the SVG document
   switch (shape) {
     case "circle":
-      svg.circle(100, 100, 50).fill(shapeColor);
+      svg.circle(100).move(100, 50).fill(shapeColor);
       break;
     case "triangle":
-      svg.polygon("100,100 150,50 200,100").fill(shapeColor);
+      svg.polygon("100,150 200,150 150,50").fill(shapeColor);
       break;
     case "square":
-      svg.rect(75, 75, 150, 150).fill(shapeColor);
+      svg.rect(100, 50, 100, 100).fill(shapeColor);
       break;
     default:
       break;
   }
 
   // Add the specified text to the SVG document
-  svg.text(150, 125, text).attr({
-    "text-anchor": "middle",
-    "font-size": "48px",
-    fill: textColor
-  });
+  svg.text(text).move(150, 125).font({ size: 48, anchor: "middle" }).fill(textColor);
 
   // Write the SVG document to a file
-  fs.writeFileSync("logo.svg", svg.toString());
+  fs.writeFileSync("logo.svg", svg.svg());
 
   console.log("Generated logo.svg");
 }
